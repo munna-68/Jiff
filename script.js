b904dae06068ddc1ac7b6be4e084813b55bd79fa
munna-68 <mahamudmunna68@gmail.com>
2026-02-13 00:02:58 +0600

    adding the giphy api and making the initial render

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const recycleButton = document.querySelector(".recycle-btn");
 const recycleIcon = document.querySelector(".recycle-icon");
+const img = document.querySelector("img");
 
-if (recycleButton && recycleIcon) {
-  recycleButton.addEventListener("click", () => {
-    recycleIcon.classList.remove("spin-once");
-    void recycleIcon.offsetWidth;
-    recycleIcon.classList.add("spin-once");
-  });
+recycleButton.addEventListener("click", () => {
+  recycleIcon.classList.remove("spin-once");
+  void recycleIcon.offsetWidth;
+  recycleIcon.classList.add("spin-once");
+});
 
-  recycleIcon.addEventListener("animationend", () => {
-    recycleIcon.classList.remove("spin-once");
+recycleIcon.addEventListener("animationend", () => {
+  recycleIcon.classList.remove("spin-once");
+});
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=9u65p4LmGldePxWFl3g47ygqlqpM8Bzb&s=cats",
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
   });
-}
