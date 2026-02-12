d0e36a19f5e4636ea5b785debb89cd3687de49e1
munna-68 <mahamudmunna68@gmail.com>
2026-02-13 00:31:54 +0600

    finishing the project with random gif showing when the button is clicked

--- script.js
+++ script.js
@@ -2,7 +2,85 @@ const recycleButton = document.querySelector(".recycle-btn");
 const recycleIcon = document.querySelector(".recycle-icon");
 const img = document.querySelector("img");
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=9u65p4LmGldePxWFl3g47ygqlqpM8Bzb&s=${getRandomPhrase()}`,
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+});
+
+const gifPhrases = [
+  "dogzoomies",
+  "confusedreaction",
+  "funnycats",
+  "epicfail",
+  "randommeme",
+  "sleepycat",
+  "laughingreaction",
+  "dogfail",
+  "awkwardmoment",
+  "animalreaction",
+  "chaosmeme",
+  "exciteddog",
+  "catstare",
+  "wtfreaction",
+  "cuteanimals",
+  "dramaticcat",
+  "randomfunny",
+  "guiltydog",
+  "unexpectedmoment",
+  "funnygif",
+  "angrycat",
+  "relatablememe",
+  "confuseddog",
+  "animalfail",
+  "memegif",
+  "dogchaos",
+  "shockedreaction",
+  "funnyanimals",
+  "catfail",
+  "weirdbutfunny",
+  "happydog",
+  "reactiongif",
+  "awkwardreaction",
+  "randomreaction",
+  "sleepydog",
+  "funnydogs",
+  "catchaos",
+  "epicreaction",
+  "randomgif",
+  "dogsideeye",
+  "confusedcat",
+  "chaosenergy",
+  "animalchaos",
+  "funnyfail",
+  "catknockover",
+  "unexpectedfail",
+  "dogconfused",
+  "memereaction",
+  "purechaos",
+];
+
+function getRandomPhrase() {
+  const randomIndex = Math.floor(Math.random() * gifPhrases.length);
+  return gifPhrases[randomIndex];
+}
+
 recycleButton.addEventListener("click", () => {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=9u65p4LmGldePxWFl3g47ygqlqpM8Bzb&s=${getRandomPhrase()}`,
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
   recycleIcon.classList.remove("spin-once");
   void recycleIcon.offsetWidth;
   recycleIcon.classList.add("spin-once");
@@ -11,13 +89,3 @@ recycleButton.addEventListener("click", () => {
 recycleIcon.addEventListener("animationend", () => {
   recycleIcon.classList.remove("spin-once");
 });
-
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=9u65p4LmGldePxWFl3g47ygqlqpM8Bzb&s=cats",
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
